ab6f3dcc09f30bd1bf781f1d81f9461e813f0e5c
CodeBruno0 <brunoaroa@gmail.com>
2023-06-07 22:02:58 -0300

    Update Pizza.java

--- aplicacao/App.java
+++ aplicacao/App.java
@@ -1,5 +1,13 @@
 package aplicacao;
 
+import model.Pizza;
+
 public class App {
-    
+
+    public static void main(String[] args) {
+        Pizza pedido = new Pizza("Queijo", 30, 'p');
+
+        System.out.println("Pizza criada!!");
+        System.out.println(pedido.PedidoIfood());
+    }
 }
